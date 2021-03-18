20e5c71f843bee989c35a5997244b1f85f116a4c
jackcusick95 <jackcusick95@gmail.com>
2021-03-18 14:14:29 -0400

    Add my_uniq rspec and method

---
+++ spec/first_tdd_spec.rb
@@ -0,0 +1,19 @@
+require "first_tdd"
+
+describe "#my_uniq" do
+
+    it "should return the uniq elements in the order in which they appear in the array" do
+    expect([1, 2, 1, 3, 3].my_uniq).to eq([1,2,3])
+    end
+
+end 
+
+describe "#two_sum" do 
+
+    it "should find all pairs of positions where elements at those positions sum to zero" do 
+    end 
+
+    context ""
+
+end 
+
